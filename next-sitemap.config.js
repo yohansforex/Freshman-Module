b52051252d8ac9e4dd60522f1575413a147f3151
Yohannes <yohannesdamtie34@gmail.com>
2025-04-22 17:49:39 +0300

    new file:   next-sitemap.config.js 	modified:   package-lock.json 	modified:   package.json

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: "https://freshman-module.vercel.app", // 🔁 Replace with your actual domain
+    generateRobotsTxt: true,           // Generate robots.txt file
+    sitemapSize: 7000,                 // Optional: split sitemap if needed
+  };
+  
