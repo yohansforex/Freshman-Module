74bfc39de2049af3cb4c80a355095b6a567be5ce
Yohannes <yohannesdamtie34@gmail.com>
2025-04-22 18:18:27 +0300

    modified:   next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-    siteUrl: "https://freshman-module.vercel.app", // 🔁 Replace with your actual domain
-    generateRobotsTxt: true,           // Generate robots.txt file
-    sitemapSize: 7000,                 // Optional: split sitemap if needed
-  };
+export const siteUrl = "https://freshman-module.vercel.app";
+export const generateRobotsTxt = true;
+export const robotsTxtOptions = {
+    policies: [
+        { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+        "https://freshman-module.vercel.app/sitemap.xml",
+    ],
+};
   
